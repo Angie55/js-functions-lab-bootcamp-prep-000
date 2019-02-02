ad2e0b2afc1984104c457485cc8e133b49966e1c
Angela Stead <angie_stead55@yahoo.com>
2019-02-02 19:08:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // write your code below!
 function happyHolidays() {
-  return "happyHolidays";
+  return "Happy Holidays";
 }
 
 function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!';
 }
+happysHolidaysTo('you!');
 
 function happyHolidayTo(holiday, name) {
   return 'Happy ${holidays}, ${name}!';
 }
-
+happyHolidayTo('Independence day', 'you!')
 function holidayCountdown(days, holidays) {
   return 'It\'s ${days} until ${holidy}!';
 }
