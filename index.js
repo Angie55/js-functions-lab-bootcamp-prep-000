cf301f42edb75acac948b4f5eab87e3c243f664b
Angela Stead <angie_stead55@yahoo.com>
2019-02-02 18:58:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "happyHolidays";
+}
+
+function happyHolidaysTo() {
+  return 'Haay holidays, ${name}!';
+}
+
+function holidayCountdown(days, holidays) {
+  return 'It
+}
